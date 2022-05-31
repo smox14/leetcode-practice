a642973fcab89fb1b52765259ba690c81b43c2cf
smox14 <kantarat.htkkw@gmail.com>
2022-05-31 10:26:25 +1000

    add test case

--- 14-longest_common_prefix/longestCommonPrefix.test.js
+++ 14-longest_common_prefix/longestCommonPrefix.test.js
@@ -8,4 +8,13 @@ describe('longestCommonPrefix', () => {
     
     expect(actualResult).toBe(expectResult)
   })
+
+  it('returns the common prefix among the input strings', () => {
+    const input = ["dog","racecar","car"]
+    const expectResult = ""
+    const actualResult = longestCommonPrefix(input)
+    
+    expect(actualResult).toBe(expectResult)
+  })
+
 })
