25d2d771c2065d27e161e5a3f16b91fdd55af4af
smox14 <kantarat.htkkw@gmail.com>
2022-05-31 22:34:00 +1000

    add hasmap solution

--- 387-first_unique_character_in_a_string/firstUniqChar.js
+++ 387-first_unique_character_in_a_string/firstUniqChar.js
@@ -6,14 +6,29 @@
 
 function firstUniqChar(s) {
   for( let i = 0; i < s.length; i++){
-    // if(!s.replace(s[i],'').includes(s[i])) {
-      if(s.indexOf(s[i]) == s.lastIndexOf(s[i])){
+    if(s.indexOf(s[i]) == s.lastIndexOf(s[i])){
       return i
     }
   }
   return -1
 }
 
+// using Hash map 
+
+function firstUniqCharHashMap(s) {
+ let charMap = new Map()
+
+ for( let i = 0; i < s.length; i++) {
+   (charMap.has(s[i]))? charMap.set(s[i], 2) : charMap.set(s[i],1)
+ }
+ 
+ let letters = Array.from(charMap.keys())
+ 
+ for(let i =0; i < letters.length; i++) {
+  if(charMap.get(letters[i]) === 1) return i
+}
+  return -1
+}
 
 
-module.exports = firstUniqChar
+module.exports = firstUniqCharHashMap
