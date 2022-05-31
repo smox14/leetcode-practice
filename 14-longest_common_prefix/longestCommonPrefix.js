3a6600ec5ef17cde2d387b91eaeb725f7fa15677
smox14 <kantarat.htkkw@gmail.com>
2022-05-31 10:20:44 +1000

    refactor code

--- 14-longest_common_prefix/longestCommonPrefix.js
+++ 14-longest_common_prefix/longestCommonPrefix.js
@@ -1,5 +1,25 @@
+// steps
+// 1. find the min lenght of the strs 
+// 2. loop to every word to check the common character
+//  if they have the common charater => store in the prefix variable, check the next character 
+//  else break the loop
+// 3. return the longest prefix
+
 function longestCommonPrefix(strs) {
-  return "fl"
+  let prefix = ''
+  // const minLength = strs.reduce((a,b) => a.length < b.length? a : b).length   
+  // or 
+  const minLength = Math.min(...strs.map( word => word.length))
+  for(let i = 0 ; i < minLength ; i++){
+    let char = strs[0][i]
+    if(strs.every( word => word[i] == char)) {
+      prefix += char
+    }else {
+      break
+    }
+    
+  }
+  return prefix
 }
 
 module.exports = longestCommonPrefix
