d2231128f17e36db6f2783f4dd9d70c4dd8347f7
smox14 <kantarat.htkkw@gmail.com>
2022-06-01 11:17:32 +1000

    add more testcase

--- 1480-running_sum/runningSum.test.js
+++ 1480-running_sum/runningSum.test.js
@@ -9,4 +9,23 @@ describe('runningSum', () =>{
 
     expect(actualResult).toEqual(expectResult);
   });
+
+  it('Should return a running sum of an array', () => {
+    const input = [1,1,1,1,1];
+    const expectResult = [1,2,3,4,5];
+
+    const actualResult = runningSum(input);
+
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('Should return a running sum of an array', () => {
+    const input = [3,1,2,10,1];
+    const expectResult = [3,4,6,16,17];
+
+    const actualResult = runningSum(input);
+
+    expect(actualResult).toEqual(expectResult);
+  });
+
 }); 
