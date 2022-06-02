b89ebfd4a59fb558d62f0eb2df619f16f1f20d23
smox14 <kantarat.htkkw@gmail.com>
2022-06-02 10:59:35 +1000

    add test case

--- 867-transpose_matrix/transpose.test.js
+++ 867-transpose_matrix/transpose.test.js
@@ -8,4 +8,14 @@ describe('transpose', () => {
     const actualResult = transpose(inputMatrix)
     expect(actualResult).toEqual(expectResult)
   });
+
+  it('Should return transpose of matrix', () => {
+    const inputMatrix =  [[1,2,3],[4,5,6]]
+    const expectResult = [[1,4],[2,5],[3,6]]
+
+    const actualResult = transpose(inputMatrix)
+    expect(actualResult).toEqual(expectResult)
+  });
+
+
 });
