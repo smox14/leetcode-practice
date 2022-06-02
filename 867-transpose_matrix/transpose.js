2bdda8c03ec463c3ac9f1d09cc4479f30bb0f5dc
smox14 <kantarat.htkkw@gmail.com>
2022-06-02 10:43:02 +1000

    add transpose matrix

---
+++ 867-transpose_matrix/transpose.js
@@ -0,0 +1,6 @@
+const transpose = function(matrix) {
+  return [[1,4,7],[2,5,8],[3,6,9]]
+
+}
+
+module.exports = transpose
---
+++ 867-transpose_matrix/transpose.test.js
@@ -0,0 +1,11 @@
+const transpose = require('./transpose')
+
+describe('transpose', () => {
+  it('Should return transpose of matrix', () => {
+    const inputMatrix = [[1,2,3],[4,5,6],[7,8,9]]
+    const expectResult = [[1,4,7],[2,5,8],[3,6,9]]
+
+    const actualResult = transpose(inputMatrix)
+    expect(actualResult).toEqual(expectResult)
+  });
+});
