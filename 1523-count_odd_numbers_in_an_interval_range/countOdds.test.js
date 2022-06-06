2dec13f115a386565445c1b684a7ba461b28f32c
smox14 <kantarat.htkkw@gmail.com>
2022-06-06 10:55:29 +1000

    add another test case

--- 1523-count_odd_numbers_in_an_interval_range/countOdds.test.js
+++ 1523-count_odd_numbers_in_an_interval_range/countOdds.test.js
@@ -9,4 +9,13 @@ describe('1523 - count odd', () => {
 
     expect(actualResult).toBe(expectResult);
   });
+
+  it('Should return the count of odd numbet between low and high (inclusive)', () => {
+    const low = 8, high = 10
+    const expectResult = 1;
+
+    const actualResult = countOdds(low, high);
+
+    expect(actualResult).toBe(expectResult);
+  });
 });
