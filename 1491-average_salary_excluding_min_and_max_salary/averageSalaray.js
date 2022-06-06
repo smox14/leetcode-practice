5cf23d9e94af2db60877a76ce46af3742211987a
smox14 <kantarat.htkkw@gmail.com>
2022-06-06 11:34:53 +1000

    refactor code

--- 1491-average_salary_excluding_min_and_max_salary/averageSalaray.js
+++ 1491-average_salary_excluding_min_and_max_salary/averageSalaray.js
@@ -1,5 +1,19 @@
+//  step 
+//  1. find min & max salary 
+//  2. delete min & max salary 
+//  3. calculate the average salary
+
 const average = function(salary) {
-  return 2500.00000
+  const min = Math.min(...salary)
+  const max = Math.max(...salary)
+  salary.splice(salary.indexOf(max),1)
+  salary.splice(salary.indexOf(min),1)
+
+  return salary
+  .reduce( (total, value) => total + value)
+  / salary.length
+  
+  
 }
 
 module.exports = average
