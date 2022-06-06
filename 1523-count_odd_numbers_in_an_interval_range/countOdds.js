2856ef63248cba84928707f16f00ebfb24544e68
smox14 <kantarat.htkkw@gmail.com>
2022-06-06 11:08:23 +1000

    add short solution

--- 1523-count_odd_numbers_in_an_interval_range/countOdds.js
+++ 1523-count_odd_numbers_in_an_interval_range/countOdds.js
@@ -9,4 +9,14 @@ const countOdds = function(low, high) {
   return totalOdd;
 };
 
-module.exports = countOdds;
+//  best short solution 
+// possible value 
+// 1. even & even
+// 2. even & odd
+// 3. odd & even 
+// 4. odd & odd ** 
+const countOdds2 = function(low, high) {
+  const diffNumber = Math.round((high - low) / 2);  // cover 1 - 3
+  return (low % 2 == 1 && high % 2 == 1)? diffNumber + 1 : diffNumber;
+};
+module.exports = countOdds2;
