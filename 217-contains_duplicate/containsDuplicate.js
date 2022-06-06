3f1b5437cc18e9b215117546ac451dd2894e4e7b
smox14 <kantarat.htkkw@gmail.com>
2022-06-06 13:02:23 +1000

    edited return value

--- 217-contains_duplicate/containsDuplicate.js
+++ 217-contains_duplicate/containsDuplicate.js
@@ -2,13 +2,13 @@
 // 1. loop through the input array 
 // 2. check the index of number 
 //  2.1 if the index of that number != the last index of that number, return true 
-// 2.2 else, check the next number 
+//  2.2 else, check the next number 
 
 const containsDuplicate = input => {
   for( let number of input){
     if(input.indexOf(number) != input.lastIndexOf(number)) return true
   }
-  return null
+  return false
 };
 
 module.exports = containsDuplicate;
