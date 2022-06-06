78159a08e52f1ba4b1cd60a523aceaac6683f4a8
smox14 <kantarat.htkkw@gmail.com>
2022-06-06 10:54:33 +1000

    refactor code

--- 1523-count_odd_numbers_in_an_interval_range/countOdds.js
+++ 1523-count_odd_numbers_in_an_interval_range/countOdds.js
@@ -1,5 +1,12 @@
 const countOdds = function(low, high) {
-  return 3;
+  let totalOdd = 0
+  for(let i = low; i <= high; i++){
+    if(i % 2 == 1){
+      totalOdd += 1
+      i++
+    }
+  }
+  return totalOdd;
 };
 
 module.exports = countOdds;
